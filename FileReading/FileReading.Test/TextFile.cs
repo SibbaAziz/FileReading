e9eacf350c52861b2f06ba3b42783f9bbe57703b
Aziz SIBBA <a.sibba@hotmail.fr>
2018-02-05 16:15:43 +0100

    Version 6

--- FileReading/FileReading.Test/TextFile.cs
+++ FileReading/FileReading.Test/TextFile.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FileReading.Reading.Text;
 using FileReading.Encryption;
+using FileReading.Security;
+using System.Collections.Generic;
 
 namespace FileReading.Test
 {
@@ -33,5 +35,68 @@ namespace FileReading.Test
             Assert.AreEqual("Test text file reading", text);
         }
 
+        [TestMethod]
+        public void Admin_Read()
+        {
+            //Arrange
+            ISecurity security = new RoleBasedSecurity(Role.Admin);
+            var reader = new TextRoleBasedFileReader(security);
+
+            //Act 
+            var xml = reader.Read("Text.txt");
+            //Assert 
+            Assert.AreEqual("Test text file reader", xml);
+        }
+
+        [TestMethod]
+        public void Authorized_User_Read()
+        {
+            //Arrange
+            List<string> accessibleFiles = new List<string>()
+            {
+                "Text.txt"
+            };
+            ISecurity security = new RoleBasedSecurity(Role.User, accessibleFiles);
+
+            var reader = new TextRoleBasedFileReader(security);
+
+            //Act 
+            var xml = reader.Read("Text.txt");
+            //Assert 
+            Assert.AreEqual("Test text file reader", xml);
+        }
+
+        [TestMethod]
+        public void UnAuthorized_User_Read()
+        {
+            //Arrange
+
+            ISecurity security = new RoleBasedSecurity(Role.User, null);
+
+            var reader = new TextRoleBasedFileReader(security);
+
+            //Act 
+            var xml = reader.Read("Text.txt");
+            //Assert 
+            Assert.AreEqual("", xml);
+        }
+
+        [TestMethod]
+        public void UnAuthorized_Different_File_User_Read()
+        {
+            //Arrange
+            List<string> accessibleFiles = new List<string>()
+            {
+                "DifferentFile.txt"
+            };
+            ISecurity security = new RoleBasedSecurity(Role.User, accessibleFiles);
+
+            var reader = new TextRoleBasedFileReader(security);
+
+            //Act 
+            var xml = reader.Read("Text.txt");
+            //Assert 
+            Assert.AreEqual("", xml);
+        }
     }
 }
---
+++ FileReading/FileReading/Reading/Text/TextRoleBasedFileReader.cs
@@ -0,0 +1,23 @@
+﻿using FileReading.Security;
+
+namespace FileReading.Reading.Text
+{
+    public class TextRoleBasedFileReader : IRoleBasedFileReader
+    {
+        private ISecurity _security;
+        private TextFileReader _reader;
+        public TextRoleBasedFileReader(ISecurity security)
+        {
+            _security = security;
+        }
+        public string Read(string path)
+        {
+            if (!_security.CanRead(path))
+                return string.Empty; // Or Throw Exception
+
+            if (_reader == null)
+                _reader = new TextFileReader();
+            return _reader.Read(path);
+        }
+    }
+}
