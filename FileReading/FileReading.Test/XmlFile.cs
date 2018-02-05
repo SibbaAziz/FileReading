e62afe4d244b65d161733352b34230042a0db205
Aziz SIBBA <a.sibba@hotmail.fr>
2018-02-05 13:52:22 +0100

    Version 2

---
+++ FileReading/FileReading.Test/XmlFile.cs
@@ -0,0 +1,26 @@
+﻿using FileReading.Reading.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileReading.Test
+{
+
+    [TestClass]
+    public class XmlFile
+    {
+        [TestMethod]
+        public void Read()
+        {
+            //Arrange
+            var reader = new XmlFileReader();
+
+            //Act 
+            var xml = reader.Read("Test.xml");
+            //Assert 
+            Assert.AreEqual(
+                            "<Person>" +
+                            "<Id>1</Id>" +
+                            "<Name>Paul</Name>" +
+                            "</Person>", xml);
+        }
+    }
+}
---
+++ FileReading/FileReading/Reading/Xml/XmlFileReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+
+namespace FileReading.Reading.Xml
+{
+    public class XmlFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            var str = new StringBuilder();
+            try
+            {
+                XmlTextReader reader = new XmlTextReader(path);
+
+                if (reader == null)
+                    return str.ToString();
+                    
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            str.Append("<" + reader.Name);
+                            str.Append(">");
+                            break;
+                        case XmlNodeType.Text: //Display the text in each element.
+                            str.Append(reader.Value);
+                            break;
+                        case XmlNodeType.EndElement: //Display the end of the element.
+                            str.Append("</" + reader.Name);
+                            str.Append(">");
+                            break;
+                    }
+                }
+
+                return str.ToString();
+            }
+            catch (Exception)
+            {
+                // Logger
+                throw;
+            }
+        }
+    }
+}
