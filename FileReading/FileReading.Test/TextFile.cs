fee6ab1ebd3ffdf1b2bd623c92e82c400d86d8c3
Aziz SIBBA <a.sibba@hotmail.fr>
2018-02-05 13:40:16 +0100

    Version 1

---
+++ FileReading/FileReading.Test/TextFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileReading.Reading.Text;
+
+namespace FileReading.Test
+{
+    [TestClass]
+    public class TextFile
+    {
+        [TestMethod]
+        public void Read()
+        {
+            //Arrange
+            var reader = new TextFileReader();
+
+            //Act 
+            var text = reader.Read("Text.txt");
+            //Assert 
+            Assert.AreEqual("Test text file reader", text);
+        }
+    }
+}
